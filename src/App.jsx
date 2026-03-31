01c4691513b732d18d640462a3aa7bc500ac4b42
manas <youremail@example.com>
2026-03-31 13:52:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+// import React from 'react'
+
+// const App = () => {
+//   let user = "Manas"
+
+//    const changeUser = () =>{
+//     console.log(user)
+//     user = "Blaze"
+//     console.log(user)
+//    }
+
+//   return (
+//     <div>
+//       <h1>Username is {user} </h1>
+//       <button onClick={changeUser}>Change User</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//HOOKS -special type of functions
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   const [a, setA] = useState(10)
+//   const changeA = ()=>{
+//     console.log("working")
+//     setA(14)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Value of a is {a}</h1>
+//       <button onClick={changeA}>ChangeA</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React, { useState } from 'react'
+// const App = () => {
+
+//   const [num, setNum] = useState(0)
+
+//   return (
+//     <div>
+//       <h1>Number is {num}</h1>
+//       <button onClick={()=>setNum(num+10)}>Increment</button>
+//       <button onClick={()=>setNum(num-10)}>Decrement</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h2 className='text-2xl bg-red-700 text-white'>Hello guys</h2>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+const App = () => {
+  const submitHandler = (e)=>{
+    e.preventDefault()
+
+    console.log("submitted")
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input className='px-4 py-3 text-xl m-5' type="text"  />
+        <button className='px-4 text-white py-3 m-5 text-xl'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
